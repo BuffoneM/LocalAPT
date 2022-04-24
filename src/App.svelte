469ddbf2057f0a799a476b21ce28671159809718
Michael Buffone <44306347+BuffoneM@users.noreply.github.com>
2022-04-24 17:39:32 -0400

    Implement svelte-table component

--- src/App.svelte
+++ src/App.svelte
@@ -1,24 +1,67 @@
+<link rel='stylesheet' href='/global.css'>
+
 <script lang="ts">
-	import AptTable from './AptTable.svelte'
+	import AptTable from './AptTable.svelte';
+	// create greeting based on time of day
+	const username = 'user';
+	const greeting = new Date().getHours() < 12 ? `Good morning, ${ username } ☀` : 
+		new Date().getHours() < 18 ? `Good afternoon, ${ username } 🌤` : `Good evening, ${ username } 🌖`;
 </script>
 
 <main>
-	<h1>Hello, user.</h1>
-	<AptTable></AptTable>
+	<div>
+		<navbar>
+			<h2>Local Apt Discovery 🚀</h2>
+		</navbar>
+		<h1> {greeting} </h1>
+		<AptTable></AptTable>
+		<p></p>
+		<footer></footer>
+	</div>
 </main>
 
 <style>
 	main {
 		text-align: center;
-		padding: 1em;
 		max-width: 240px;
-		margin: 0 auto;
 	}
 
 	h1 {
-		color: #ff3e00;
+		color: var(--c1);
 		font-size: 4em;
 		font-weight: 100;
+	}
+	
+	h2 {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		padding-left: 0.5em;
+		color: var(--c1);
+		font-size: 1.5em;
+		font-weight: 100;
+	}
+	
+	navbar {
+		position: static;
+		top: 0;
+		width: 100%;
+		height: 50px;
+		display: flex;
+		justify-content: space-between;
+		border-bottom: 1px solid var(--c1);
+		background: linear-gradient(to left, var(--c4), var(--c5));
+	}
+	
+	footer {
+		position: fixed;
+		bottom: 0;
+		width: 100%;
+		height: 50px;
+		display: flex;
+		justify-content: space-between;
+		border-top: 1px solid var(--c1);
+		background: linear-gradient(to right, var(--c4), var(--c5));
 	}
 
 	@media (min-width: 500px) {
--- src/AptTable.svelte
+++ src/AptTable.svelte
@@ -1,11 +1,81 @@
-<script>
+<link rel='stylesheet' href='/global.css'>
 
+<script>
+    import SvelteTable from "svelte-table";
+    const rows = [
+        { id: 1, first_name: "Marilyn", last_name: "Monroe", pet: "dog" },
+        { id: 2, first_name: "Abraham", last_name: "Lincoln", pet: "dog" },
+        { id: 3, first_name: "Mother", last_name: "Teresa", pet: "dino" },
+        { id: 4, first_name: "John F.", last_name: "Kennedy", pet: "dog" },
+        { id: 5, first_name: "Martin Luther", last_name: "King", pet: "dog" },
+        { id: 6, first_name: "Nelson", last_name: "Mandela", pet: "cat" },
+        { id: 7, first_name: "Winston", last_name: "Churchill", pet: "cat" },
+        { id: 8, first_name: "George", last_name: "Soros", pet: "bird" },
+        { id: 9, first_name: "Bill", last_name: "Gates", pet: "cat" },
+        { id: 10, first_name: "Muhammad", last_name: "Ali", pet: "dog" },
+        { id: 11, first_name: "Mahatma", last_name: "Gandhi", pet: "bird" },
+        { id: 12, first_name: "Margaret", last_name: "Thatcher", pet: "cat" }
+    ];
+    const columns = [
+        {
+            key: "id",
+            title: "ID",
+            value: (v) => v.id,
+            sortable: true,
+        },
+        {
+            key: "first_name",
+            title: "FIRST_NAME",
+            value: (v) => v.first_name,
+            sortable: true,
+        },
+        {
+            key: "last_name",
+            title: "LAST_NAME",
+            value: (v) => v.last_name,
+            sortable: true,
+        },
+        {
+            key: "pet",
+            title: "Pet",
+            value: (v) => v.pet,
+            sortable: true,
+        },
+    ];
 </script>
 
 <main>
-    <h1>table</h1>
+    <div>
+        <table>
+            <SvelteTable {columns} {rows}/>
+        </table>
+    </div>
 </main>
 
 <style>
+    table {
+        margin: 5em;
+        padding: 2.5rem;
+        background-color: var(--c5);
+        border: 1px solid var(--c2);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border-radius: 15px;
+    }
 
+    :global(table td) {
+        color: var(--c1);
+        text-align: left;
+        padding: 1rem;
+        border-radius: 2.5px;
+    }
+
+    :global(table tbody tr:nth-child(odd)) {
+        background-color: var(--c3);
+    }
+
+    :global(table tbody tr:nth-child(even)) {
+        background-color: var(--c4);
+    }
 </style>
